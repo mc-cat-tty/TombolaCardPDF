77aa17f0973a5e11e14c76e46c6d9332d3a391bf
mc-cat-tty <>
2021-01-01 23:10:36 +0100

    new names; pdf print enhanced

--- card.py
+++ card.py
@@ -5,11 +5,10 @@ Module containing class Card and some method for its management
 from typing import List
 
 class Card:
-    def __init__(self, row_list: List[List], title: str, bottom_text: str) -> None:
+    def __init__(self, row_list: List[List], title: str) -> None:
         self.content: List[List] = row_list
         self.title: str = title
         self.id: int = self.__get_number(self.title)
-        self.bottom_text: str = bottom_text
 
     def __get_number(self, s: str) -> int:
         for n in s.split():
--- pdf_formatter.py
+++ pdf_formatter.py
@@ -10,16 +10,15 @@ from jinja2 import Template, Environment, FileSystemLoader
 from card import Card, CardsSet
 from typing import List
 
-HTML_TEMPLATE_FILENAME = "card_template.html"
-HTML_FILENAME = "card.html"
-PDF_FILENAME = "card.pdf"
-LOGO_FILENAME = "logo.jpeg"
-BOTTOM_TEXT = "test_txt"
+HTML_TEMPLATE_FILENAME: str = "cards_set_template.html"
+HTML_FILENAME: str = "cards_set.html"
+PDF_FILENAME: str = "cards_set.pdf"
+LOGO_FILENAME: str = "logo.jpeg"
+BOTTOM_TEXT: str = "test_txt"
 
 def transpose_matrix(original_matrix: List[List]) -> List[List]:
     original_matrix_lens: List[int] = [len(col) for col in original_matrix]
     max_col_len: int = max(original_matrix_lens)
-    # print(max_col_len)
     for col in original_matrix:
         if len(col) < max_col_len:
             col.extend([-1] * (max_col_len - len(col)))
@@ -32,14 +31,11 @@ def import_data_from(source: List[str]) -> CardsSet:
     for i, line_pair in enumerate(zip(source, source[1:])):
         if not i%2:
             title: str = line_pair[0]
-            # print(f"title: {title}")
             if not line_pair[1]:
                 continue
             cols_list: List[List] = ast.literal_eval(line_pair[1])
-            # print(f"cols_list: {cols_list}")
             rows_list: List[List] = transpose_matrix(cols_list)
-            # print(f"rows_list: {rows_list}")
-            cards_set.append(Card(rows_list, title, BOTTOM_TEXT))
+            cards_set.append(Card(rows_list, title))
     return cards_set
 
 
@@ -48,14 +44,21 @@ def main() -> None:
     for line in sys.stdin:
         buf.append(line.strip())
     cards_set: CardsSet = import_data_from(buf)
-    # print(cards_set[0].content)
+    
+    # Jinja 2 configuration
     template_loader = FileSystemLoader(searchpath="./")  # Place files inside the same directory
     template_env = Environment(loader=template_loader)
     template = template_env.get_template(HTML_TEMPLATE_FILENAME)
     with open(HTML_FILENAME, "w") as f:
-        text = template.render(card=cards_set[0], logo_filename=LOGO_FILENAME)
+        text = template.render(cards_set=cards_set, logo_filename=LOGO_FILENAME, bottom_text=BOTTOM_TEXT, enumerate=enumerate)
         f.write(text)
-    pdfkit.from_file(HTML_FILENAME, PDF_FILENAME)
+    
+    # pdfkit configuration
+    options = {
+            "page-size": "A4",
+            "enable-local-file-access": None
+    }
+    pdfkit.from_file(HTML_FILENAME, PDF_FILENAME, options=options)
 
 if __name__ == "__main__":
     main()
